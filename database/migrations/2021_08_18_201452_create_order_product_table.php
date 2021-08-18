0db2d1c76b1c8f4c826f2eb71c11ccb294a707b6
Eduardo <eduardodev08@gmail.com>
2021-08-18 16:37:12 -0400

    renamed migration detail_order to order_product

--- database/migrations/2021_08_18_201452_create_order_product_table.php
+++ database/migrations/2021_08_18_201452_create_order_product_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDetailsOrdersTable extends Migration
+class CreateOrderProductTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateDetailsOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('details_orders', function (Blueprint $table) {
+        Schema::create('order_product', function (Blueprint $table) {
             $table->id();
             $table->foreignIdent('order_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
             $table->foreignIdent('product_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
             $table->integer('quantity');
-            $table->double('unit_price', 8,2);
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateDetailsOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('details_orders');
+        Schema::dropIfExists('order_product');
     }
 }
