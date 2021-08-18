9b62a2d2db5679b9daca80da487b5817bfd3813a
Eduardo <eduardodev08@gmail.com>
2021-08-18 16:13:01 -0400

    created migration for orders table and rename migration for sub-category table

--- database/migrations/2021_08_18_195504_create_table_sub_categories.php
+++ database/migrations/2021_08_18_195504_create_table_sub_categories.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTableSubCategories extends Migration
+class CreateSubCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTableSubCategories extends Migration
      */
     public function up()
     {
-        Schema::create('table_sub_categories', function (Blueprint $table) {
+        Schema::create('sub_categories', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255);
             $table->text('description');
