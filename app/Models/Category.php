f7a97c53b184d11cc786351993f9ecbe3a1f2c1a
Eduardo <eduardodev08@gmail.com>
2021-08-18 17:35:52 -0400

    creating and linking category and subCategories models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -14,4 +14,8 @@ class Category extends Model
     public function products(){
         return $this->hasMany(Product::class);
     }
+
+    public function subCategories(){
+        return $this->hasMany(SubCategories::class);
+    }
 }
